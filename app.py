5974f94576a85e6ea36c17f871c37a1a8c8e2722
yaput <antonius.putra@babel.jp>
2021-09-25 14:26:38 +0700

    Add new test case, add state to simple-process

--- app.py
+++ app.py
@@ -6,15 +6,17 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello world"
+    return "Hello world, asdfsdaf"
 
 
 @app.route("/simple-process")
 def simple_process_handler():
-    first_param = int(request.args.get('first'))
-    second_param = int(request.args.get('second'))
+    first_param = int(request.args.get('x'))
+    second_param = int(request.args.get('y'))
 
     return str(simple_process(first_param, second_param))
 
-def simple_process(first, second):
+def simple_process(first, second, state=""):
+    if state == "negate":
+        return -1 * (first * second + second)
     return first * second + second
